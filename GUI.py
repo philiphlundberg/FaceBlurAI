6e2541c38f90ab6d5a8b18ce4acbe2cfa22c9d76
philiphlundberg <154505705+philiphlundberg@users.noreply.github.com>
2024-08-05 09:59:00 +0200

    Add files via upload

---
+++ GUI.py
@@ -0,0 +1,108 @@
+import tkinter as tk
+from tkinter import filedialog
+import configparser
+import subprocess  # Add this import
+
+
+#### GUI FOR THE FACEBLUR AI PROGRAM ####
+
+# Add a global flag to track if the input start entry has been created
+input_start_created = False
+input_duration_created = False
+
+def save_config():
+    global input_start_created, input_duration_created # Declare both as global
+    config['VideoSettings']['input_path'] = input_path_entry.get()
+    config['Blurring']['threshold'] = str(value_slider.get())
+
+    if keep_audio_var.get():
+        config['VideoSettings']['keep_audio'] = 'true'
+    else:
+        config['VideoSettings']['keep_audio'] = 'false'
+
+    if cut_video_var.get():
+        config['VideoSettings']['cut_video'] = 'true'
+        
+        # Check if the input_start_label already exists
+        if not input_start_created:  # Use the flag to check
+            global input_start_label, input_start_entry  # Declare as global to modify outside the function
+            input_start_label = tk.Label(root, text="Start time of video:", bg='lightblue', fg='black')  # Customize colors
+            input_start_label.pack()
+            input_start_entry = tk.Entry(root, width=50, bg='white', fg='black')  # Customize colors
+            input_start_entry.insert(0, config['VideoSettings']['start_time'])
+            input_start_entry.pack()
+            input_start_created = True  # Set the flag to True after creation
+
+        # Check if the input_start_label already exists
+        if not input_duration_created:  # Use the flag to check
+            global input_duration_label, input_duration_entry  # Declare as global to modify outside the function
+            input_duration_label = tk.Label(root, text="Duration:", bg='lightblue', fg='black')  # Customize colors
+            input_duration_label.pack()
+            input_duration_entry = tk.Entry(root, width=50, bg='white', fg='black')  # Customize colors
+            input_duration_entry.insert(0, config['VideoSettings']['duration'])
+            input_duration_entry.pack()
+            input_duration_created = True  # Set the flag to True after creation
+        
+        config['VideoSettings']['start_time'] = input_start_entry.get()
+        config['VideoSettings']['duration'] = input_duration_entry.get()
+    else:
+        config['VideoSettings']['cut_video'] = 'false'
+    
+    
+    with open('config.ini', 'w') as configfile:
+        config.write(configfile)
+
+
+def start_other_script():  
+    subprocess.Popen(['python', 'working.py'])  # Replace 'working.py' with your file name
+    root.destroy()  # Close the config editor GUI
+
+# Load config file
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+# Create GUI
+root = tk.Tk()
+root.title("FaceBlur")
+root.geometry("500x400")  # Set the window size to 400x300 pixels
+root.configure(bg='lightblue')  # Set background color for the main window
+
+input_path_label = tk.Label(root, text="Path to video:", bg='lightblue', fg='black')  # Customize colors
+input_path_label.pack()
+input_path_entry = tk.Entry(root, width=50, bg='white', fg='black')  # Customize colors
+input_path_entry.insert(0, config['VideoSettings']['input_path'])
+input_path_entry.pack()
+
+# Frame for Cut Video
+cut_video_frame = tk.Frame(root, bg='lightblue')  # Create a frame for cut video
+cut_video_frame.pack(side=tk.LEFT, padx=50)  # Align to the left with padding
+cut_video_label = tk.Label(cut_video_frame, text="Cut video:", bg='lightblue', fg='black')  # Customize colors
+cut_video_label.pack(pady=5)  # Pack label with vertical padding
+cut_video_var = tk.BooleanVar(root)
+cut_video_var.set(config['VideoSettings'].getboolean('cut_video'))
+cut_video_checkbox = tk.Checkbutton(cut_video_frame, text="Yes", variable=cut_video_var, bg='lightblue', fg='black')  # Customize colors
+cut_video_checkbox.pack(pady=5)  # Pack checkbox with vertical padding
+
+keep_audio_frame = tk.Frame(root, bg='lightblue')  # Create a frame for cut video
+keep_audio_frame.pack(side=tk.RIGHT, padx=50)  # Align to the right with padding
+keep_audio_label = tk.Label(keep_audio_frame, text="Keep audio:", bg='lightblue', fg='black')  # Customize colors
+keep_audio_label.pack(pady=5)  # Pack label with vertical padding
+keep_audio_var = tk.BooleanVar(root)
+keep_audio_var.set(config['VideoSettings'].getboolean('keep_audio'))
+keep_audio_checkbox = tk.Checkbutton(keep_audio_frame, text="Yes", variable=keep_audio_var, bg='lightblue', fg='black')  # Customize colors
+keep_audio_checkbox.pack(pady=5)  # Pack checkbox with vertical padding
+
+slider_label = tk.Label(root, text="Set Confidence Level:", bg='lightblue', fg='black')  # Customize colors
+slider_label.pack()
+value_slider = tk.Scale(root, from_=0.0, to=1.0, resolution=0.1, orient=tk.HORIZONTAL, bg='lightblue', fg='black')  # Customize colors
+value_slider.set(0.3)  # Set default value to 0.3
+value_slider.pack()
+
+# Positioning buttons using place()
+save_button = tk.Button(root, text="Save Configurations", command=save_config, bg='green', fg='white')  # Customize colors
+save_button.place(x=90, y=300)  # Set x and y coordinates
+
+start_button = tk.Button(root, text="Run Blurring Script", command=start_other_script, bg='blue', fg='white')  # Customize colors
+start_button.place(x=300, y=300)  # Set x and y coordinates
+
+root.mainloop()
---
+++ working.py
@@ -0,0 +1,256 @@
+import cv2
+from ultralytics import YOLO
+import numpy as np
+import time
+import configparser
+import os
+import subprocess
+
+
+TIME_PER_FRAME = 0.0031355378818845 #minutes
+
+
+class VideoCutter:
+    def __init__(self, input_path, ffmpeg_path='ffmpeg'):
+        self.input_path = input_path
+        self.ffmpeg_path = ffmpeg_path
+
+    def cut_video(self, start_time, duration):
+        import subprocess
+        import os
+
+        # Ensure ffmpeg path is correct
+        if not os.path.isfile(self.ffmpeg_path):
+            raise FileNotFoundError(f"ffmpeg executable not found at {self.ffmpeg_path}")
+
+        # Generate output path based on input path
+        output_dir, input_filename = os.path.split(self.input_path)
+        output_filename = 'cut_' + input_filename
+        self.output_path = os.path.join(output_dir, output_filename)
+        
+        # Check if the output file already exists and overwrite it
+        if os.path.exists(self.output_path):
+            os.remove(self.output_path)  # Remove the existing file
+
+        # ffmpeg command to cut the video
+        command = [
+            self.ffmpeg_path,        # Full path to ffmpeg executable
+            '-i', self.input_path,   # Input file
+            '-ss', str(start_time),  # Start time
+            '-t', str(duration),     # Duration
+            '-c:v', 'copy',          # Copy video codec (no re-encoding)
+            '-c:a', 'copy',          # Copy audio codec (no re-encoding)
+            self.output_path         # Output file
+        ]
+        subprocess.run(command, check=True)
+
+        # Check if the output file was created
+        if not os.path.exists(self.output_path):
+            raise FileNotFoundError(f"Output file {self.output_path} was not created.")
+
+####### Read the config file to get the user inputs ########
+config = configparser.ConfigParser()
+config.sections()
+config.read('config.ini')
+settings = config['VideoSettings']
+input_path = settings['input_path']
+ffmpeg_path = settings['ffmpeg_path']
+cut_video = settings['cut_video']
+start_time = settings['start_time']
+duration = settings['duration']
+keep_audio = settings['keep_audio']
+
+blur = config['Blurring']
+confidence_threshold = float(blur['threshold'])
+
+
+video_path = input_path
+
+###### THIS IS THE START OF THE PROGRAM #######
+print("\n\nLoading video... \n\n")
+
+if cut_video == 'true':
+    print("Cutting video...\n")
+    cutter = VideoCutter(video_path, ffmpeg_path)
+    cutter.cut_video(int(start_time), int(duration))
+    video_path = cutter.output_path
+    
+    print("\nThe video was successfully cut.\n")
+
+
+# YOLO model
+model = YOLO('yolov8n-face.pt')
+
+# Create a VideoCapture object
+cap = cv2.VideoCapture(video_path)
+file_size = os.path.getsize(video_path)  # Get file size in bytes
+
+# Check if the video opened successfully
+if not cap.isOpened():
+    print("Error: Invalid path, could not open video.")
+    exit()
+
+# Get the width and height of the frames
+frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+time_per_frame = TIME_PER_FRAME
+fps = int(cap.get(cv2.CAP_PROP_FPS))
+total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))  # Ensure this is after reinitializing cap
+print(f"File size: {1000 * os.path.getsize(video_path)} kB")
+print(f"Total frames in the video: {total_frames}")
+print(f"Estimated time is {round(time_per_frame * total_frames, 2)} minutes")
+
+# Ask user if they want to continue
+# Clear any previous output before asking for input
+print("\n")  # Optional: Add a newline for clarity
+user_input = input("Tap 'Enter' three times to accept or exit the program").strip().lower()
+if user_input != '':
+    print("Exiting the program.")
+    exit()
+else: 
+    print("\nInitializing inference...\n")
+
+
+# Generate output path based on input path
+video_output_dir, video_filename = os.path.split(video_path)
+video_filename = 'blurred_' + video_filename
+
+# Check if the video filename already exists, in that case, remove it
+if os.path.exists(os.path.join(video_output_dir, video_filename)):
+    os.remove(os.path.join(video_output_dir, video_filename))
+
+# Create the new video path
+video_output_path = os.path.join(video_output_dir, video_filename)
+
+
+# Define the codec and create a VideoWriter object
+out = cv2.VideoWriter(video_output_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (frame_width, frame_height))
+
+# Define the class you want to filter (e.g., class 0)
+desired_class = 0
+
+# Create a buffer to store bounding boxes for consecutive frames
+bbox_buffer = []
+
+# Initialize a variable to store the start time
+start_time = time.time()
+
+# Read until the video is completed
+while cap.isOpened():
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    
+    if ret:
+        # Process the frame with YOLO model in streaming mode with confidence threshold
+        results = model(frame, stream=True, conf=confidence_threshold)
+        
+        # Store current frame's bounding boxes
+        current_bboxes = []
+        for result in results:
+            for box in result.boxes:
+                if box.cls == desired_class:  # Filter by desired class
+                    x1, y1, x2, y2 = map(int, box.xyxy[0])
+                    current_bboxes.append((x1, y1, x2, y2))
+        
+        # Add current frame's bounding boxes to the buffer
+        bbox_buffer.append(current_bboxes)
+        
+        # Keep only the last 3 frames in the buffer
+        if len(bbox_buffer) > 3:
+            bbox_buffer.pop(0)
+        
+        # Apply blur to bounding boxes from the buffer
+        for bboxes in bbox_buffer:
+            for (x1, y1, x2, y2) in bboxes:
+                # Scale the bounding box dimensions by 1.2
+                scale_factor = 1.6
+                new_width = int((x2 - x1) * scale_factor)
+                new_height = int((y2 - y1) * scale_factor)
+                new_x1 = max(0, x1 - (new_width - (x2 - x1)) // 2)
+                new_y1 = max(0, y1 - (new_height - (y2 - y1)) // 2)
+                new_x2 = min(frame.shape[1], new_x1 + new_width)
+                new_y2 = min(frame.shape[0], new_y1 + new_height)
+                bbox = (new_x1, new_y1, new_x2 - new_x1, new_y2 - new_y1)
+                
+                # Extract the region of interest (ROI)
+                roi = frame[new_y1:new_y2, new_x1:new_x2]
+                
+                # Calculate the kernel size based on the bounding box size
+                kernel_size = max(51, int(min(bbox[2], bbox[3]) / 10) * 2 + 1)
+                
+                # Apply gaussian blur to the ROI
+                blurred_roi = cv2.GaussianBlur(roi, (kernel_size, kernel_size), 0)
+                
+                # Create a mask for the ellipse
+                mask = np.zeros_like(roi)
+                center = (bbox[2] // 2, bbox[3] // 2)
+                axes = (bbox[2] // 2, bbox[3] // 2)
+                cv2.ellipse(mask, center, axes, 0, 0, 360, (255, 255, 255), -1)
+                
+                # Combine the blurred ROI with the original frame using the mask
+                frame[new_y1:new_y2, new_x1:new_x2] = np.where(mask == 255, blurred_roi, roi)
+        
+        # Write the frame into the output video
+        out.write(frame)
+        
+        # Press Q on keyboard to exit or close the window
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+# When everything done, release the video capture and writer objects
+cap.release()
+out.release()
+
+
+
+# Closes all the frames
+cv2.destroyAllWindows()
+
+if keep_audio == 'true':
+    print("\nExtracting the audio from the original video...\n")
+
+    # Generate output path based on input path
+    audio_dir, audio_filename = os.path.split(video_path)
+    audio_filename = 'w_audio_' + os.path.basename(video_output_path)  # Use basename of video_output_path
+
+    # Check if the audio filename already exists, in that case, remove it
+    if os.path.exists(os.path.join(audio_dir, audio_filename)):
+        os.remove(os.path.join(audio_dir, audio_filename))
+
+    # Create the new audio output path
+    video_audio_output_path = os.path.join(audio_dir, audio_filename)
+
+    # Extract audio from the original video
+    audio_output_path = 'audio.aac'  # Temporary audio file
+    subprocess.run([ffmpeg_path, '-i', video_path, '-q:a', '0', '-map', 'a', audio_output_path])
+
+    # Combine the processed video with the original audio
+    output_video_with_audio = os.path.join(audio_dir, video_audio_output_path)  # Define the output path
+    subprocess.run([ffmpeg_path, '-i', video_path, '-i', audio_output_path, '-c:v', 'copy', '-c:a', 'aac', output_video_with_audio])
+
+    # Clean up temporary audio file
+    os.remove(audio_output_path)
+
+    # Calculate the elapsed time
+    elapsed_time = time.time() - start_time
+    print("\n\n#######################################\n\n")
+    print("\n\nFinished processing video...\n")
+    print("Saving the output files to the same folder as the input video...")
+    print(f"Elapsed time: {elapsed_time/60} minutes")
+    print(f"\nOutput file with audio saved as: {output_video_with_audio}\n")
+
+else:
+    # Calculate the elapsed time
+    elapsed_time = time.time() - start_time
+    print("\n\nFinished blurring video...\n")
+    print("Saving the output file to the same folder as the input video...")
+    print(f"Output file name: {video_output_path}")
+    print(f"Elapsed time: {elapsed_time/60} minutes\n")
+
+# Closes all the frames
+cv2.destroyAllWindows()
+
+# Add this line to pause the console
+input("Press 'Enter' to exit the program... ")  # This will keep the console open until you press Enter
